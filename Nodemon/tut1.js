b0851c1768c689b00f55f818b51b5a10d4672699
tanvi-radia <139433835+tanvi-radia@users.noreply.github.com>
2023-09-17 16:41:29 +0530

    Add files via upload

---
+++ Nodemon/tut1.js
@@ -0,0 +1,7 @@
+// Nodemon:
+// npm install: installs nodemodules
+// Installation Steps:
+// 1. npm init: creates package.json file
+// 2. npm i nodemon --save-dev: installs nodemon
+// 3. npm i nodemon -g: installs nodemon globally
+console.log("This is Tanvi Radia");
